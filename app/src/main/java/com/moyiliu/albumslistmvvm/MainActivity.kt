269f90a894ac836a6428cd9b01a1aaf6b9e53be3
Moyi Liu <moyidev@gmail.com>
2018-12-28 14:27:43 +0000

    [Implemented] Album list in MainActivity (Planning to change to one activity many fragment architecture) [Updated] dimension file.

--- app/src/main/java/com/moyiliu/albumslistmvvm/MainActivity.kt
+++ app/src/main/java/com/moyiliu/albumslistmvvm/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.moyiliu.albumslistmvvm
 
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import androidx.navigation.NavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.moyiliu.albumslistmvvm.base.BaseActivity
+import com.moyiliu.albumslistmvvm.base.BaseAdapter
 import com.moyiliu.albumslistmvvm.base.ViewModelFactory
 import com.moyiliu.albumslistmvvm.base.viewModel
+import com.moyiliu.albumslistmvvm.databinding.ActivityMainBinding
+import com.moyiliu.albumslistmvvm.lifecycle.observe
 import com.moyiliu.albumslistmvvm.viewmodel.AlbumViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), SwipeRefreshLayout.OnRefreshListener {
 
 //    override lateinit var navController: NavController
 
@@ -17,8 +24,28 @@ class MainActivity : BaseActivity() {
 
     val viewModel by viewModel<AlbumViewModel>{ viewModelFactory }
 
+    private val adapter = BaseAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindContentView<ActivityMainBinding>(R.layout.activity_main){
+            viewModel = this@MainActivity.viewModel
+        }
+        albumRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = this@MainActivity.adapter
+
+            layoutAnimation = AnimationUtils.loadLayoutAnimation(this@MainActivity, R.anim.layout_animation_from_bottom)
+        }
+
+        albumSwipeRefreshLayout.setOnRefreshListener(this)
+
+        viewModel.albums.observe(this){
+            adapter.dataList = it
+        }
+    }
+
+    override fun onRefresh() {
+        viewModel.refreshAlbums()
     }
 }
